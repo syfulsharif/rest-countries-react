92198b61dda4bb55debea7157a305bda6c4fcc34
syfulsharif <sharif.syful@gmail.com>
2021-09-25 11:31:09 +0600

    New Folder Structure Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import Countries from './components/Countries/Countries';
+
 
 function App() {
   return (
@@ -10,30 +11,30 @@ function App() {
   );
 }
 
-function Countries () {
-  const [countries, setCountries] = useState([]);
-  useEffect(() => {
-    fetch('https://restcountries.com/v2/all')
-      .then(res => res.json())
-      .then(data => setCountries(data));
-  }, []);
-  return (
-    <div>
-      <h4>Traveling Around The World!</h4>
-      <h6>Countries Found {countries.length}</h6> {
-        countries.map(country => <Country name={country.name} capital={country.capital}></Country>)
-      }
-    </div>
-  )
-}
+// function Countries () {
+//   const [countries, setCountries] = useState([]);
+//   useEffect(() => {
+//     fetch('https://restcountries.com/v2/all')
+//       .then(res => res.json())
+//       .then(data => setCountries(data));
+//   }, []);
+//   return (
+//     <div>
+//       <h4>Traveling Around The World!</h4>
+//       <h6>Countries Found {countries.length}</h6> {
+//         countries.map(country => <Country name={country.name} capital={country.capital}></Country>)
+//       }
+//     </div>
+//   )
+// }
 
-function Country (props) {
-  return (
-    <div>
-      <h2>Name : {props.name}</h2>
-      <h3>Capital : {props.capital}</h3>
-    </div>
-  )
-}
+// function Country (props) {
+//   return (
+//     <div>
+//       <h2>Name : {props.name}</h2>
+//       <h3>Capital : {props.capital}</h3>
+//     </div>
+//   )
+// }
 
 export default App;
---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+
+const Countries = () => {
+    const [countries, setCountries] = useState([]);
+    useEffect(() => {
+        fetch('https://restcountries.com/v2/all')
+            .then(res => res.json())
+            .then(data => setCountries(data));
+    }, [])
+    return (
+        <div>
+            <h1>Hello From Total Countries {countries.length}</h1>
+        </div>
+    );
+};
+
+export default Countries;
